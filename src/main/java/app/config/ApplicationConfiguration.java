4c1181654859dc2f37426fc605cfcbbae603f9ba
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2020-02-24 00:15:16 +0200

    Commit

--- src/main/java/app/config/ApplicationConfiguration.java
+++ src/main/java/app/config/ApplicationConfiguration.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
 @EnableScheduling
 @ComponentScan(basePackages = "app")
-public class ApplicationConfiguration {
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
 
-	public void addResourceHandlers() {
-		
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
 	}
-	
+
 }
