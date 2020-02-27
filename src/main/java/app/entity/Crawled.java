47d170d11baad2f485b8879d8772547084b93c11
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2020-02-27 17:45:47 +0200

    Add order by for images

--- src/main/java/app/entity/Crawled.java
+++ src/main/java/app/entity/Crawled.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -57,6 +58,7 @@ public class Crawled {
 	private CrawledRating crawledRating;
 
 	@OneToMany(mappedBy = "crawled", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@OrderBy
 	private Set<CrawledImage> crawledImages;
 
 	@ManyToMany(fetch = FetchType.LAZY)
