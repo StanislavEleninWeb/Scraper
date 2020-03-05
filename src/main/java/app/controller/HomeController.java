531d8ecb5accc77de2dd9c1011fa63ddca96e845
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2020-03-05 23:45:50 +0200

    Add search criteria

--- src/main/java/app/controller/HomeController.java
+++ src/main/java/app/controller/HomeController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import app.entity.Crawled;
+import app.entity.UserCriteria;
+import app.enumerated.SearchOperation;
+import app.repository.specs.CrawledSpecification;
+import app.repository.specs.SearchCriteria;
 import app.service.BuildTypeService;
 import app.service.CrawledService;
 import app.service.ResidenceTypeService;
@@ -41,11 +45,39 @@ public class HomeController {
 	public String index(@PageableDefault(size = 20, sort = "id", direction = Direction.DESC) Pageable pageable,
 			Model model) {
 
-		model.addAttribute("userCriteria", userCriteriaService.findByUserAndPrimary(userService.findById(1), true));
 		model.addAttribute("residenceType", residenceTypeService.findAll());
 		model.addAttribute("buildType", buildTypeService.findAll());
 
-		Page<Crawled> crawled = crawledService.findAll(pageable);
+		UserCriteria criteria = userCriteriaService.findByUserAndPrimary(userService.findById(1), true);
+		model.addAttribute("userCriteria", criteria);
+
+		CrawledSpecification specification = new CrawledSpecification();
+
+		if (criteria.getPriceMin() != null)
+			specification.add(new SearchCriteria("price", criteria.getPriceMin(), "crawledInfo",
+					SearchOperation.GREATER_THAN_EQUAL));
+
+		if (criteria.getPriceMax() != null)
+			specification.add(new SearchCriteria("price", criteria.getPriceMax(), "crawledInfo",
+					SearchOperation.LESS_THAN_EQUAL));
+
+		if (criteria.getPricePerSquareMin() != null)
+			specification.add(new SearchCriteria("pricePerSquare", criteria.getPricePerSquareMin(), "crawledInfo",
+					SearchOperation.GREATER_THAN_EQUAL));
+
+		if (criteria.getPricePerSquareMax() != null)
+			specification.add(new SearchCriteria("size", criteria.getPricePerSquareMax(), "crawledInfo",
+					SearchOperation.LESS_THAN_EQUAL));
+
+		if (criteria.getSizeMin() != null)
+			specification.add(new SearchCriteria("size", criteria.getSizeMin(), "crawledInfo",
+					SearchOperation.GREATER_THAN_EQUAL));
+
+		if (criteria.getSizeMax() != null)
+			specification.add(
+					new SearchCriteria("size", criteria.getSizeMax(), "crawledInfo", SearchOperation.LESS_THAN_EQUAL));
+
+		Page<Crawled> crawled = crawledService.findAll(specification, pageable);
 		model.addAttribute("crawled", crawled);
 
 		String sort = crawled.getSort().stream().map(order -> order.getProperty() + "," + order.getDirection())
